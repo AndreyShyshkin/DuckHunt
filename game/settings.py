dff999b520e242d5c7724890994d95d3ffda9093
AndreyShyshkin <shyshkinandrey06@gmail.com>
2026-02-04 15:12:37 +0200

    feat: add paths var in settings.py

--- game/settings.py
+++ game/settings.py
@@ -4,3 +4,9 @@ import os
 ORIG_W = 800
 ORIG_H = 500
 
+# Paths
+ASSETS_DIR = 'assets'
+IMAGES_DIR = os.path.join(ASSETS_DIR, 'images')
+AUDIO_DIR = os.path.join(ASSETS_DIR, 'audio')
+FONTS_DIR = os.path.join(ASSETS_DIR, 'fonts')
+
