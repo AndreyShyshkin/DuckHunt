c75e82df4eb8231d6ae5d863a0b4533e0876d939
AndreyShyshkin <shyshkinandrey06@gmail.com>
2026-02-08 23:46:37 +0200

    bug fix full screen

--- src/duckhunt/app.py
+++ src/duckhunt/app.py
@@ -9,6 +9,7 @@ class Game(object):
     def __init__(self, width, height, fullscreen=False):
         self.running = True
         self.surface = None
+        self.game_surface = None
         self.clock = pygame.time.Clock()
         self.size = width, height
         self.fullscreen = fullscreen
@@ -18,20 +19,51 @@ class Game(object):
         self.background = pygame.transform.smoothscale(bg, self.size)
         self.driver = None
 
+        self.window_size = self.size
+        self.scale = 1.0
+        self.scale_pos = (0, 0)
+
         self.accumulator = 0.0
         self.logic_update_rate = 50.0
         self.dt = 1.0 / self.logic_update_rate
 
     def init(self):
+        if not self.fullscreen:
+            os.environ['SDL_VIDEO_CENTERED'] = '1'
+
         flags = pygame.FULLSCREEN if self.fullscreen else 0
-        self.surface = pygame.display.set_mode(self.size, flags)
-        self.driver = driver.Driver(self.surface)
+        if self.fullscreen:
+            self.surface = pygame.display.set_mode((0, 0), flags)
+        else:
+            self.surface = pygame.display.set_mode(self.size, flags)
+
+        self.window_size = self.surface.get_size()
+
+        self.game_surface = pygame.Surface(self.size).convert_alpha()
+        self.driver = driver.Driver(self.game_surface)
+
+        self._recompute_scale()
+
+    def _recompute_scale(self):
+        window_w, window_h = self.window_size
+        game_w, game_h = self.size
+        scale = min(window_w / game_w, window_h / game_h)
+        self.scale = scale
+        scaled_w = int(game_w * scale)
+        scaled_h = int(game_h * scale)
+        pos_x = (window_w - scaled_w) // 2
+        pos_y = (window_h - scaled_h) // 2
+        self.scale_pos = (pos_x, pos_y)
+        self.scaled_size = (scaled_w, scaled_h)
 
     def handleEvent(self, event):
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             self.running = False
         else:
             self.driver.handleEvent(event)
+            if event.type == pygame.VIDEORESIZE:
+                self.window_size = event.size
+                self._recompute_scale()
 
     def loop(self):
         frame_time = self.clock.tick(settings.FRAMES_PER_SEC) / 1000.0
@@ -46,8 +78,16 @@ class Game(object):
             self.accumulator -= self.dt
 
     def render(self):
-        self.surface.blit(self.background, (0,0))
+        self.game_surface.blit(self.background, (0, 0))
         self.driver.render()
+
+        if self.scaled_size != self.size:
+            scaled = pygame.transform.smoothscale(self.game_surface, self.scaled_size)
+        else:
+            scaled = self.game_surface
+
+        self.surface.fill((0, 0, 0))
+        self.surface.blit(scaled, self.scale_pos)
         pygame.display.flip()
 
     def cleanup(self):
